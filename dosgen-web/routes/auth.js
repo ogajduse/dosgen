4e9269d0f95f8d6f2ceb44c89104ceb2ab93b96f
Ondřej Gajdušek <ogajduse@redhat.com>
2018-05-18 16:43:56 +0200

    reformat all javascript code

--- dosgen-web/routes/auth.js
+++ dosgen-web/routes/auth.js
@@ -1,24 +1,21 @@
-module.exports = function (passport) {
-    var express = require("express");
-    var router = express.Router();
-    //Presmerovanie k prihlasovaciemu formuláru
-    router.get("/", function (req, res, next) {
-        res.redirect("/auth/login/");
+module.exports = function(passport) {
+  var express = require("express");
+  var router = express.Router();
+  //Presmerovanie k prihlasovaciemu formuláru
+  router.get("/", function(req, res, next) {
+    res.redirect("/auth/login/");
+  });
+  //Presmerovanie pri úspešnom/neúspešnom prihlásení
+  router.post("/login", passport.authenticate("local", {
+    failureRedirect: "/auth/login?status=fail",
+    successRedirect: "/dosgen"
+  }));
+  //Akcia pri neúspešnom prihlásení
+  router.get("/login", function(req, res, next) {
+    var status = req.query.status;
+    res.render('login', {
+      failedLogin: status == "fail"
     });
-    //Presmerovanie pri úspešnom/neúspešnom prihlásení
-    router.post("/login", passport.authenticate("local", {
-        failureRedirect: "/auth/login?status=fail",
-        successRedirect: "/dosgen"
-    }));
-    //Akcia pri neúspešnom prihlásení
-    router.get("/login", function (req, res, next) {
-        var status = req.query.status;
-        res.render('login', {failedLogin: status == "fail"});
-    });
-    return router;
+  });
+  return router;
 };
-
-
-
-
-
