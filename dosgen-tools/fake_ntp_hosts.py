716574626810366589086cdff061028f55e02374
Ondřej Gajdušek <ogajduse@redhat.com>
2018-05-26 17:21:58 +0200

    use different ip

--- dosgen-tools/fake_ntp_hosts.py
+++ dosgen-tools/fake_ntp_hosts.py
@@ -6,4 +6,4 @@ faker = Faker()
 for i in range(600):
     ip_addr = faker.ipv4()
     ntp_data_pattern = "\x17\x00\x03\x2a" + "\x00" * 4
-    send(IP(dst="192.168.124.14", src=ip_addr)/fuzz(UDP()/NTP(version=4)), iface="enp7s0")
+    send(IP(dst="192.168.124.223", src=ip_addr)/fuzz(UDP()/NTP(version=4)), iface="enp7s0")
