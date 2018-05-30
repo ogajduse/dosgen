4567487118075000682d73900a89f998841d049f
Ondřej Gajdušek <ogajduse@redhat.com>
2018-05-30 07:06:37 +0200

    deleted some files, before finishing the thesis

--- dosgen-tools/get_monlist.py
+++
@@ -1,6 +0,0 @@
-from scapy.all import *
-ntp_data_pattern = "\x17\x00\x03\x2a" + "\x00" * 4
-
-packet = IP(dst="192.168.124.14",src="192.168.124.129")/UDP(sport=51147,dport=123)/Raw(load=ntp_data_pattern)
-
-send(packet,iface="enp7s0")
